603093d70a3733809a00adee7991c72e4040f3f3
Mark Engelberg <mark.engelberg@gmail.com>
2017-05-26 19:35:32 -0700

    Updated to alpha17

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "http://github.com/Engelberg/better-cond"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]])
